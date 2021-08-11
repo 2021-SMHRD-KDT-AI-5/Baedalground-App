589479e61bf46d16d93d2fd0b767b36475025daa
김대원 <85005828+kdw6158@users.noreply.github.com>
2021-08-11 16:40:07 +0900

    3rd 프로젝트 로그인 확인

--- app/src/main/java/com/example/a3project/Login.java
+++ app/src/main/java/com/example/a3project/Login.java
@@ -1,20 +1,38 @@
 package com.example.a3project;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Login extends AppCompatActivity {
 
     EditText edt_id, edt_pw;
     Button btn_login;
     TextView tv_gojoin;
+
+    RequestQueue requestQueue;
+    StringRequest stringRequest_join;
+    StringRequest stringRequest_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +46,52 @@ public class Login extends AppCompatActivity {
 
         memberVO member = new memberVO();
 
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        stringRequest_login = new StringRequest(Request.Method.POST, "http://172.30.1.54:8090/p3_server/LoginServlet", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.equals("1")) {
+                    Intent it_login = new Intent(Login.this, MainActivity.class);
+                    it_login.putExtra("id", edt_id.getText().toString());
+                    Toast.makeText(getApplicationContext(), "로그인성공!", Toast.LENGTH_SHORT).show();
+                    startActivity(it_login);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
+                error.printStackTrace();
+            }
+        })
+        {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+
+                params.put("id", edt_id.getText().toString());
+                params.put("pw", edt_pw.getText().toString());
+
+                return params;
+            }
+        };
+
+
+
+
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(edt_id.equals(member.getId()) && edt_pw.equals(member.getPw())){
-//                    Intent it = new Intent(); // main 으로 이동할 Intent
-//                    it.putExtra("id", R.id.edt_id);
-                }else{
-                    // 로그인 실패시 toast
-                    Toast.makeText(Login.this, "로그인을 다시 실행해주세요!", Toast.LENGTH_LONG).show();
-                }
+//                if(edt_id.equals(member.getId()) && edt_pw.equals(member.getPw())){
+////                    Intent it = new Intent(); // main 으로 이동할 Intent
+////                    it.putExtra("id", R.id.edt_id);
+//                }else{
+//                    // 로그인 실패시 toast
+//                    Toast.makeText(Login.this, "로그인을 다시 실행해주세요!", Toast.LENGTH_LONG).show();
+//                }
+                requestQueue.add(stringRequest_login);
             }
         });
 
--- app/src/main/java/com/example/a3project/MainActivity.java
+++ app/src/main/java/com/example/a3project/MainActivity.java
@@ -8,7 +8,6 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
