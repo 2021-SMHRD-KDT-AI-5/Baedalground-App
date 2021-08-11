f7d01343a1859a1c3914e0c1d22525a5e58acb66
e-u-n0607 <ming6297@naver.com>
2021-08-11 16:33:27 +0900

    - JOIN 체크박스, 비밀번호 확인 수정 - JOIN, LOGIN 로고 수정 - drawable 'teamimg.png'이미지 수정

--- app/src/main/java/com/example/a3project/Join.java
+++ app/src/main/java/com/example/a3project/Join.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -13,9 +14,40 @@ import android.widget.Toast;
 
 public class Join extends AppCompatActivity {
 
+
+
     EditText edt_joinid, edt_joinpw, edt_checkpw, edt_address, edt_age;
     CheckBox cb_man, cb_women;
-    Button btn_join;
+    Button btn_check, btn_join;
+    String gender = null;
+
+//    public void checked(View v){
+//        cb_man = findViewById(R.id.cb_man);
+//        cb_women = findViewById(R.id.cb_women);
+//
+//
+//        cb_man.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (cb_man.isChecked()) {
+//                    cb_women.setChecked(false);
+//                    gender = cb_man.getText().toString();
+//                }
+//            }
+//        });
+//
+//
+//        cb_women.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(cb_women.isChecked()){
+//                    cb_man.setChecked(false);
+//                    gender = cb_women.getText().toString();
+//                }
+//            }
+//        });
+//    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,38 +59,86 @@ public class Join extends AppCompatActivity {
         edt_checkpw = findViewById(R.id.edt_checkpw);
         edt_address = findViewById(R.id.edt_address);
         edt_age = findViewById(R.id.edt_age);
-        cb_man = findViewById(R.id.cb_man);
-        cb_women = findViewById(R.id.cb_woman);
+        btn_check = findViewById(R.id.btn_check);
         btn_join = findViewById(R.id.btn_join);
+
+        cb_man = findViewById(R.id.cb_man);
+        cb_women = findViewById(R.id.cb_women);
+
+
+        cb_man.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cb_man.isChecked()) {
+                    cb_women.setChecked(false);
+                    gender = cb_man.getText().toString();
+                }
+            }
+        });
+
+
+        cb_women.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(cb_women.isChecked()){
+                    cb_man.setChecked(false);
+                    gender = cb_women.getText().toString();
+                }
+            }
+        });
+
+        String id = edt_joinid.getText().toString();
+        String pw = edt_joinpw.getText().toString();
+        String address = edt_address.getText().toString();
+        String age = edt_age.getText().toString();
+//        String gender = check;
+
+        btn_check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                String pw = edt_joinpw.getText().toString();
+                Log.d("test", pw+"//"+edt_checkpw.getText());
+                if(edt_joinpw.getText().toString().equals(edt_checkpw.getText().toString())){
+                    Toast.makeText(Join.this, "비밀번호가 일치합니다.", Toast.LENGTH_LONG).show();
+                    btn_join.setClickable(true);
+                }else{
+                    // 다음으로 못넘어가게
+                    Toast.makeText(Join.this, "비밀번호가 다릅니다!", Toast.LENGTH_LONG).show();
+                    btn_join.setClickable(false);
+                }
+            }
+        });
+
+
+
+
 
         btn_join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String id = edt_joinid.getText().toString();
+                String pw = edt_joinpw.getText().toString();
+                String address = edt_address.getText().toString();
+                String age = edt_age.getText().toString();
+
                 memberVO member = new memberVO();
-                member.setId(edt_joinid.getText().toString());
 
-                // 비밀번호 확인
-                if(edt_joinpw.equals(edt_checkpw)){
-                    member.setPw(edt_joinpw.getText().toString());
-                }else{
-                    Toast.makeText(Join.this, "비밀번호가 다릅니다!", Toast.LENGTH_LONG).show();
-                }
+                member.setId(id);
+                member.setPw(pw);
 
-                member.setAddress(edt_address.getText().toString());
-                member.setAge(edt_age.getText().toString());
-
-                // 체크박스 구분
-                if(cb_man.isChecked()){
-                    member.setGender(cb_man.getText().toString());
-                }
-                if(cb_women.isChecked()){
-                    member.setGender(cb_women.getText().toString());
-                }
+                member.setAddress(address);
+                member.setAge(age);
+                member.setGender(gender);
 
                 // vo에 값 셋팅 후 로그인 페이지 이동
                 Intent it = new Intent();
                 finish();
             }
         });
+
+
     }
+
+
 }
