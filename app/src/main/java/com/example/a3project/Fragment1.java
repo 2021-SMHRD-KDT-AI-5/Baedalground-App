0bae1a221d00e297d10b28e0cdedab2b6354907c
dong424z <dong424z@daum.net>
2021-08-12 16:35:44 +0900

    세부 디자인 수정

--- app/src/main/java/com/example/a3project/Fragment1.java
+++ app/src/main/java/com/example/a3project/Fragment1.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class Fragment1 extends Fragment {
@@ -14,9 +16,21 @@ public class Fragment1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        //프래그먼트 화면1
+        EditText edt_select;
+        Button btn_select;
         View view = inflater.inflate(R.layout.fragment_1, container, false);
+        edt_select = view.findViewById(R.id.edt_select);
+        btn_select = view.findViewById(R.id.btn_select);
+
+        btn_select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt_select.getText().toString();
+
+            }
+        });
+
         return view;
-        //test2
+
     }
 }
