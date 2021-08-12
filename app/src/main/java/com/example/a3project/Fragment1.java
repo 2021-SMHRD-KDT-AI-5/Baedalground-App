adf55ea8ef0d3739b1eb976f43370cdeeea258ca
Huigeun <yoonjk5965@naver.com>
2021-08-12 09:45:00 +0900

    희근 테스트

--- app/src/main/java/com/example/a3project/Fragment1.java
+++ app/src/main/java/com/example/a3project/Fragment1.java
@@ -17,5 +17,6 @@ public class Fragment1 extends Fragment {
         //프래그먼트 화면1
         View view = inflater.inflate(R.layout.fragment_1, container, false);
         return view;
+        //test
     }
 }
