9aa2a7f643fd9d9c1d59ca0775a903db0212e6f9
dong424z <dong424z@daum.net>
2021-08-12 11:29:06 +0900

    세부 디자인 수정

--- app/src/main/java/com/example/a3project/Fragment6.java
+++ app/src/main/java/com/example/a3project/Fragment6.java
@@ -8,19 +8,63 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class Fragment6 extends Fragment {
 
+    ImageView img_1,img_2,img_3,img_4,img_5,img_6;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_6, container, false);
 
+        img_1 = view.findViewById(R.id.img_1);
+        img_2 = view.findViewById(R.id.img_2);
+        img_3 = view.findViewById(R.id.img_3);
+        img_4 = view.findViewById(R.id.img_4);
+        img_5 = view.findViewById(R.id.img_5);
+        img_6 = view.findViewById(R.id.img_6);
 
+        img_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        img_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        img_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        img_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        img_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        img_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
         return view;
